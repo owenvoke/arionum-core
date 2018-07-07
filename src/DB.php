d23f2820fad4bfd200d551c1c7a832c0c5401f89
pxgamer <owzie123@gmail.com>
2018-07-07 22:19:47 +0100

    Add PhpDoc comments to all DB methods

--- src/DB.php
+++ src/DB.php
@@ -12,51 +12,76 @@ use PDOException;
  */
 class DB extends PDO
 {
+    /**
+     * @var string
+     */
     private $error;
+    /**
+     * @var string
+     */
     private $sql;
+    /**
+     * @var array|string
+     */
     private $bind;
+    /**
+     * @var int
+     */
     private $debugger = 0;
-    public $working = "yes";
+    /**
+     * @var string
+     */
+    public $working = 'yes';
 
-    public function __construct(string $dsn, string $user = "", string $passwd = "", int $debug_level = 0)
+    /**
+     * DB constructor.
+     * @param string $dsn
+     * @param string $user
+     * @param string $password
+     * @param int    $debugLevel
+     */
+    public function __construct(string $dsn, string $user = '', string $password = '', int $debugLevel = 0)
     {
         $options = [
             PDO::ATTR_PERSISTENT       => true,
             PDO::ATTR_EMULATE_PREPARES => false,
             PDO::ATTR_ERRMODE          => PDO::ERRMODE_EXCEPTION,
         ];
-        $this->debugger = $debug_level;
+        $this->debugger = $debugLevel;
         try {
-            parent::__construct($dsn, $user, $passwd, $options);
+            parent::__construct($dsn, $user, $password, $options);
         } catch (PDOException $e) {
             $this->error = $e->getMessage();
-            die("Could not connect to the DB - ".$this->error);
+            die('Could not connect to the DB - '.$this->error);
         }
     }
 
-    private function debug()
+    /**
+     * Debug the database connection.
+     */
+    private function debug(): void
     {
         if (!$this->debugger) {
             return;
         }
-        $error = ["Error" => $this->error];
+        $error = ['Error' => $this->error];
         if (!empty($this->sql)) {
-            $error["SQL Statement"] = $this->sql;
+            $error['SQL Statement'] = $this->sql;
         }
         if (!empty($this->bind)) {
-            $error["Bind Parameters"] = trim(print_r($this->bind, true));
+            $error['Bind Parameters'] = trim(print_r($this->bind, true));
         }
 
         $backtrace = debug_backtrace();
         if (!empty($backtrace)) {
             foreach ($backtrace as $info) {
-                if ($info["file"] != __FILE__) {
-                    $error["Backtrace"] = $info["file"]." at line ".$info["line"];
+                if ($info['file'] != __FILE__) {
+                    $error['Backtrace'] = $info['file'].' at line '.$info['line'];
                 }
             }
         }
-        $msg = "";
-        $msg .= "SQL Error\n".str_repeat("-", 50);
+        $msg = '';
+        $msg .= "SQL Error\n".str_repeat('-', 50);
         foreach ($error as $key => $val) {
             $msg .= "\n\n$key:\n$val";
         }
@@ -66,7 +91,12 @@ class DB extends PDO
         }
     }
 
-    private function cleanup($bind, $sql = "")
+    /**
+     * @param array|string $bind
+     * @param string       $sql
+     * @return array
+     */
+    private function cleanup($bind, string $sql = ''): array
     {
         if (!is_array($bind)) {
             if (!empty($bind)) {
@@ -77,22 +107,28 @@ class DB extends PDO
         }
 
         foreach ($bind as $key => $val) {
-            if (str_replace($key, "", $sql) == $sql) {
+            if (str_replace($key, '', $sql) == $sql) {
                 unset($bind[$key]);
             }
         }
         return $bind;
     }
 
-    public function single($sql, $bind = "")
+    /**
+     * @param string $sql
+     * @param string $bind
+     * @return bool|mixed
+     */
+    public function single(string $sql, $bind = '')
     {
         $this->sql = trim($sql);
         $this->bind = $this->cleanup($bind, $sql);
-        $this->error = "";
+        $this->error = '';
+
         try {
-            $pdostmt = $this->prepare($this->sql);
-            if ($pdostmt->execute($this->bind) !== false) {
-                return $pdostmt->fetchColumn();
+            $pdoStatement = $this->prepare($this->sql);
+            if ($pdoStatement->execute($this->bind) !== false) {
+                return $pdoStatement->fetchColumn();
             }
         } catch (PDOException $e) {
             $this->error = $e->getMessage();
@@ -101,19 +137,25 @@ class DB extends PDO
         }
     }
 
-    public function run($sql, $bind = "")
+    /**
+     * Run a SQL statement.
+     * @param string       $sql
+     * @param array|string $bind
+     * @return array|bool|int
+     */
+    public function run(string $sql, $bind = '')
     {
         $this->sql = trim($sql);
         $this->bind = $this->cleanup($bind, $sql);
-        $this->error = "";
+        $this->error = '';
 
         try {
-            $pdostmt = $this->prepare($this->sql);
-            if ($pdostmt->execute($this->bind) !== false) {
-                if (preg_match("/^(".implode("|", ["select", "describe", "pragma"]).") /i", $this->sql)) {
-                    return $pdostmt->fetchAll(PDO::FETCH_ASSOC);
-                } elseif (preg_match("/^(".implode("|", ["delete", "insert", "update"]).") /i", $this->sql)) {
-                    return $pdostmt->rowCount();
+            $pdoStatement = $this->prepare($this->sql);
+            if ($pdoStatement->execute($this->bind) !== false) {
+                if (preg_match('/^('.implode('|', ['select', 'describe', 'pragma']).') /i', $this->sql)) {
+                    return $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
+                } elseif (preg_match('/^('.implode('|', ['delete', 'insert', 'update']).') /i', $this->sql)) {
+                    return $pdoStatement->rowCount();
                 }
             }
         } catch (PDOException $e) {
@@ -123,7 +165,13 @@ class DB extends PDO
         }
     }
 
-    public function row($sql, $bind = "")
+    /**
+     * Retrieve a row from the database.
+     * @param string       $sql
+     * @param array|string $bind
+     * @return array|bool|int|mixed
+     */
+    public function row(string $sql, $bind = '')
     {
         $query = $this->run($sql, $bind);
         if (count($query) == 0) {
