5d02d435a4e3d6c6bb044280435cc29a44ea8e3a
pxgamer <owzie123@gmail.com>
2018-08-10 22:48:39 +0100

    Add a Forks class for listing fork blocks

---
+++ src/Forks.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Arionum\Arionum;
+
+/**
+ * Class Forks
+ */
+class Forks
+{
+    /**
+     * Update the mining algorithm to make the coin more resistant to GPU mining.
+     * @link https://git.io/fNQAP#resistance-10800
+     */
+    public const RESISTANCE = 10800;
+    /**
+     * Introduce address aliasing and masternodes, as well as a new hybrid mining system.
+     * @link https://git.io/fNQAP#renaissance-80000
+     */
+    public const RENAISSANCE = 80000;
+}
